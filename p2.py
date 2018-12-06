dd7dd3fd191b4dd09574c54c95ccb4b17cfeade8
soydna <soren.oydna@mhwirth.com>
2018-12-06 08:51:36 +0100

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,83 @@
+from unityagents import UnityEnvironment
+import numpy as np
+
+# select this option to load version 1 (with a single agent) of the environment
+#env = UnityEnvironment(file_name='/data/Reacher_One_Linux_NoVis/Reacher_One_Linux_NoVis.x86_64')
+
+# select this option to load version 2 (with 20 agents) of the environment
+env = UnityEnvironment(file_name='~/p2/Reacher_Linux_NoVis/Reacher.x86_64')
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+
+# examine the state space 
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+
+import random
+import torch
+import numpy as np
+from collections import deque
+
+from ddpg_agent import Agent
+
+agents = [Agent(state_size=state_size, action_size=action_size, random_seed=2) for n in range(num_agents)]
+#agent = Agent(state_size=state_size, action_size=action_size, random_seed=2)
+
+
+def ddpg(n_episodes=200, max_t=1000):
+    scores_deque = deque(maxlen=100)
+    scores = []
+    max_score = -np.Inf
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        states = env_info.vector_observations
+        for i in range(num_agents):
+            agents[i].reset()
+        score = 0.0
+        for t in range(max_t):
+            actions = []
+            for i in range(num_agents):
+                action = actions.append(agents[i].act(states[i], add_noise= False))
+            
+            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+            next_states = env_info.vector_observations         # get next state (for each agent)
+            rewards = env_info.rewards                       # get reward (for each agent)
+            dones = env_info.local_done                       # see if episode finished
+            
+            for i in range(num_agents):
+                agents[i].step(states[i], actions[i], rewards[i], next_states[i], dones[i])
+            
+            score += sum(rewards) / num_agents
+            states = next_states
+
+            if np.any(dones):
+                break 
+                
+        scores_deque.append(score)
+        scores.append(score)
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
+        if i_episode % 100 == 0:
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+    return scores
+
+scores = ddpg()
+
+
+
